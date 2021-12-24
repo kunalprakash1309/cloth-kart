f4f0c59f2f23244838ce263ac1e828a9d97aeb21
kunal prakash <kunalprakash1309@gmail.com>
2021-12-24 14:37:24 +0530

    Added react-router-dom and learnt about different function provided by this library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Route, Switch} from "react-router-dom"
 
 import Homepage from './pages/homepage/homepage.componet';
 import './App.css';
 
+const Hats = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <Homepage />
+    <div>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/hats" component={Hats} />
+        </Switch>
+    </div>
   )
 }
 
