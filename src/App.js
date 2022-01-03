c679aca00d2f62aa87a5fb241912bb05dbd72131
kunal prakash <kunalprakash1309@gmail.com>
2022-01-03 22:42:50 +0530

    added small usable component like form-input and custom-button and styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch} from "react-router-dom"
 import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.componet';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
 
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUp} />
         </Switch>
     </div>
   )
