6cd4cdc95841f0f5170d9716eb126a3e0ceca585
kunal prakash <kunalprakash1309@gmail.com>
2022-01-09 14:04:35 +0530

    Added redirect to home when user is logged in and added user action type

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch} from "react-router-dom"
+import { Route, Switch, Redirect} from "react-router-dom"
 
 import { connect } from 'react-redux';
 
@@ -53,15 +53,32 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={Homepage} />
             <Route exact path="/shop" component={ShopPage} />
-            <Route exact path="/signin" component={SignInAndSignUp} />
+            <Route 
+              exact 
+              path="/signin" 
+              render={() => 
+                this.props.currentUser 
+                ? <Redirect to="/"/>
+                : <SignInAndSignUp/>} 
+            />
+
+            {/* By using this method we cannot pass route props to its children
+            <Route exact path="/signin" >
+              {this.props.currentUser?<Redirect to="/" />: <SignInAndSignUp /> }
+            </Route> */}
+
           </Switch>
       </div>
     )
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,9 @@
 // fucntion that return object
 // only thing is that each object is correct order in order to be
 
+import { UserActionTypes } from "./user.types";
+
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 })
