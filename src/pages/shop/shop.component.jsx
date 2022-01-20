7a7aa059cabfa83e0ce24a135c4abc1e2b81b992
kunal prakash <kunalprakash1309@gmail.com>
2022-01-20 20:43:38 +0530

    Added HOC and use it in spinner component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,11 +3,18 @@ import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
 
 import CollectionOverview from '../../components/collection-overview/collection.overview.component'
-import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils'
-import updateCollections from '../../redux/shop/shop.actions'
 import CollectionPage from '../collection/collection.component'
+import WithSpinner from '../../components/with-spinner/with-spinner.component'
 
-import "./shop.style.scss"
+import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils'
+
+import updateCollections from '../../redux/shop/shop.actions'
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
+
+
 
 // complete code is shifted to collection-overview component because now we need to 
 // add respective shop page(i.e hats, mens, etc). Because until now ShopPage only render
@@ -15,6 +22,10 @@ import "./shop.style.scss"
 
 // need to change from fumctional to class component to store and fetch shot data from firestore and pass it to its children
 class ShopPage extends React.Component{
+    state = {
+        loading: true
+    }
+
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
@@ -30,15 +41,19 @@ class ShopPage extends React.Component{
             // In simple term, we are passing whole docs refrence present in collectins collection
             const collectionsMap = convertCollectionsSnapshotToMap(snapShot)
             updateCollections(collectionsMap)
+            this.setState({
+                loading: false
+            })
         })
     }
 
     render() {
         const { match } = this.props
+        const { loading } = this.state
         return (
             <div className='shop-page' >
-                <Route exact path={`${match.path}`} component={CollectionOverview}/>
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props} />}/>
+                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} /> } />
             </div>
         )
     }
